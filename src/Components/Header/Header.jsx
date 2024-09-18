584bca052fcadf4b1199d1451e0952b88866106b
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-09-18 08:58:13 -0300

    [FIX] = Header creation and bug fixing
    Creation of the header component, addition of svg images and fixing svg image installation bugs

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import styles from "./header.module.css";
 import { Link } from "react-router-dom";
+import DogsIcon from "/src/Assets/dogs.svg";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className="container">
-        <Link to={"/"}>Home</Link>
-        <Link to={"/login"}>Login / Criar</Link>
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to={"/"} aria-label="Dogs - Home">
+          <img src={DogsIcon} alt="Dogs Icon" />
+        </Link>
+        <Link className={styles.login} to={"/login"}>
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
