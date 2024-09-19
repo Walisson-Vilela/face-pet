46c2414cd610bc3178c4f2febab43ad8d5c19bd9
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-09-19 14:48:49 -0300

    [FEAT] = Creation API Fetch component
    Creation API Fetch component

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,45 +2,54 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
-import useForm from '../../Hooks/useForm';
+import useForm from "../../Hooks/useForm";
+import { TOKEN_POST, USER_GET } from "../../Api";
 
 const LoginForm = () => {
-    const username = useForm();
-    const password = useForm();
-  
-    function handleSubmit(event) {
-      event.preventDefault();
-  
-      if (username.validate() && password.validate()) {
-        fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(),
-        })
-          .then((response) => {
-            console.log(response);
-            return response.json();
-          })
-          .then((json) => {
-            console.log(json);
-          });
-      }
+  const username = useForm();
+  const password = useForm();
+
+  React.useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    if (token) {
+      getUser(token);
     }
-  
-    return (
-      <section>
-        <h1>Login</h1>
-        <form action="" onSubmit={handleSubmit}>
-          <Input label="Usuário" type="text" name="username" {...username} />
-          <Input label="Senha" type="password" name="password" {...password} />
-          <Button>Entrar</Button>
-        </form>
-        <Link to="/login/criar">Cadastro</Link>
-      </section>
-    );
-  };
-  
-  export default LoginForm;
-  
+  }, []);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+    console.log(json);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (username.validate() && password.validate()) {
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+
+      const response = await fetch(url, options);
+      const json = await response.json();
+      window.localStorage.setItem('token', json.token);
+      getUser(json.token);
+    }
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Entrar</Button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
