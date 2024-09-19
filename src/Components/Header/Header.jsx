18016e6c45963357a94cd3edbfc864162cc41779
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-09-19 16:01:17 -0300

    [FEAT] - user-context created
    user-context created

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import styles from "./header.module.css";
 import { Link } from "react-router-dom";
 import DogsIcon from "/src/Assets/dogs.svg";
+import { UserContext } from "../../UserContext";
+
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link className={styles.logo} to={"/"} aria-label="Dogs - Home">
-          <img src={DogsIcon} alt="Dogs Icon" />
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+        <img src={DogsIcon} alt="Dogs Icon" />
         </Link>
-        <Link className={styles.login} to={"/login"}>
-          Login / Criar
-        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
