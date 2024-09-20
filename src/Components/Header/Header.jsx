9968cd1099f414f4ca6ce2555c354dbb573f388b
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-09-20 09:05:36 -0300

    [FEAT] = Creating a context for login and logout
    Adding a logout function by clicking the logout button so that the user can log out without having to return to the login page.
    Fixing bugs using navigate

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,9 +4,8 @@ import { Link } from "react-router-dom";
 import DogsIcon from "/src/Assets/dogs.svg";
 import { UserContext } from "../../UserContext";
 
-
 const Header = () => {
-  const { data } = React.useContext(UserContext);
+  const { data, userLogout } = React.useContext(UserContext);
 
   return (
     <header className={styles.header}>
@@ -17,6 +16,7 @@ const Header = () => {
         {data ? (
           <Link className={styles.login} to="/conta">
             {data.nome}
+            <button onClick={userLogout}>Sair</button>
           </Link>
         ) : (
           <Link className={styles.login} to="/login">
