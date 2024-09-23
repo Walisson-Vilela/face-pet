621eefd65b43e3928f9751069de40903ce0f765d
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-09-23 14:07:31 -0300

    [FIX] = adjustment in the redirection to logged in home
    adjustment in the redirection to logged in home

--- src/Components/Login/LoginCreate.jsx
+++ src/Components/Login/LoginCreate.jsx
@@ -3,11 +3,14 @@ import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
 import { USER_POST } from "../../api";
+import { UserContext } from '../../UserContext';
 
 const LoginCreate = () => {
   const username = useForm();
-  const email = useForm("email");
+  const email = useForm('email');
   const password = useForm();
+
+  const { userLogin } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -17,17 +20,18 @@ const LoginCreate = () => {
       password: password.value,
     });
     const response = await fetch(url, options);
-    console.log(response);
+    if (response.ok) userLogin(username.value, password.value);
   }
 
   return (
     <section className="animeLeft">
       <h1 className="title">Cadastre-se</h1>
-      <form onSubmit={handleSubmit}></form>
-      <Input label="Usuário" type="text" name="username" {...username} />
-      <Input label="Email" type="email" name="email" {...email} />
-      <Input label="Senha" type="password" name="password" {...password} />
-      <Button>Cadastrar</Button>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Email" type="email" name="email" {...email} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Cadastrar</Button>
+      </form>
     </section>
   );
 };
